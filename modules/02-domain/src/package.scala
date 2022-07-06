232128a88fa114d128459be6482c0068f178ab6f
Adrien <adrien.crovetto@gmail.com>
2022-07-06 19:15:18 +0200

    more english

--- modules/02-domain/src/package.scala
+++ modules/02-domain/src/package.scala
@@ -1,5 +1,10 @@
 package domain {
   case class URL(value: String)
   case class Percentage(value: Double)
+
+  /**
+   * All `Amount` are in cents.
+   * There are functions to help you convert the amount from euros to cents in `NumberUtils`
+   */
   case class Amount(value: Int)
 }
